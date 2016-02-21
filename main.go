10ff35da4ed88b0f9946b0b4d046d2715773c3a1
Sean Purcell <s3purcell@gmail.com>
2016-02-21 08:40:11 -0800

    add mdns discovery

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ import (
 
 func chooseDevice(controller *spotcontrol.SpircController, reader *bufio.Reader) string{
 	devices := controller.ListDevices()
+	if len(devices) == 0 {
+		fmt.Println("no devices")
+		return ""
+	}
+
 	fmt.Println("\n choose a device:")
 	for i, d := range devices {
 		fmt.Printf("%v) %v %v \n", i, d.Name, d.Ident)
@@ -37,6 +42,32 @@ func getDevice(controller *spotcontrol.SpircController, ident string, reader *bu
 	}
 }
 
+func addMdns(controller *spotcontrol.SpircController, reader *bufio.Reader) {
+	devices := controller.ListMdnsDevices()
+	if len(devices) == 0 {
+		fmt.Println("no devices found")
+		return
+	}
+	fmt.Println("\n choose a device:")
+	for i, d := range devices {
+		fmt.Printf("%v) [mdns]%v %v \n", i, d.Name, d.Url)
+	}
+	var url string
+	for {
+		fmt.Print("Enter device number: ")
+		text, _ := reader.ReadString('\n')
+		i, err := strconv.Atoi(strings.TrimSpace(text))
+		if err == nil && i < len(devices) && i >= 0{
+			url = devices[i].Url
+			break
+		}
+		fmt.Println("invalid device number")
+	}
+
+	controller.ConnectToDevice(url)
+
+}
+
 func printHelp(){
 	fmt.Println("\nAvailable commands:")
 	fmt.Println("load <track1> [...more tracks]: load tracks by spotify base 62 id")
@@ -44,6 +75,7 @@ func printHelp(){
 	fmt.Println("play:                           play current track")
 	fmt.Println("pause:                          pause playing track")
 	fmt.Println("devices:                        list availbale devices")
+	fmt.Println("mdns:                           show devices found via zeroconf, and login on device\n")
 	fmt.Println("help:                           show this list\n")
 }
 
@@ -52,23 +84,30 @@ func main() {
 	username := flag.String("username", "", "spotify username")
 	password := flag.String("password", "", "spotify password")
 	appkey := flag.String("appkey", "./spotify_appkey.key", "spotify appkey file path")
+	blobPath := flag.String("blobPath", "", "path to saved blob")
 	flag.Parse()
 
-	if *username == "" || *password == "" {
-		fmt.Println("need to supply a username and password")
+	var session *spotcontrol.Session
+	if *username != "" && *password != ""{
+		session = spotcontrol.Login(*username, *password, *appkey)
+	} else if *blobPath != "" {
+		if _, err := os.Stat(*blobPath); os.IsNotExist(err) {
+			session = spotcontrol.LoginDiscovery(*blobPath, *appkey)
+		} else {
+			session = spotcontrol.LoginBlobFile(*blobPath, *appkey)
+		}
+	} else {
+		fmt.Println("need to supply a username and password or a blob file path")
+		fmt.Println("./spirccontroller --blobPath ./path/to/blob")
+		fmt.Println("or")
 		fmt.Println("./spirccontroller --username SPOTIFY_USERNAME --password SPOTIFY_PASSWORD")
 		return
 	}
 
-	s := spotcontrol.Session{}
-	s.StartConnection()
-	s.Login(*username, *password, *appkey)
-	s.Run()
+	session.Run()
 
-
-	//fmt.Println(convert62("3Vn9oCZbdI1EMO7jxdz2Rc 2nMW1mZmdIt5rZCsX1uh9J"))
-
-	sController := spotcontrol.SetupController(&s, *username, "7288edd0fc3ffcbe93a0cf06e3568e28521687bc")
+	sController := spotcontrol.SetupController(session, *username, 
+		"7288edd0fc3ffcbe93a0cf06e3568e28521687bc")
 	
 	go sController.Run()
 	sController.SendHello()
@@ -84,17 +123,25 @@ func main() {
 		switch {
 		case cmds[0] == "load":
 			ident = getDevice(&sController, ident, reader)
-			sController.LoadTrack(ident, cmds[1:])
+			if ident != "" {
+				sController.LoadTrack(ident, cmds[1:])
+			}
 		case cmds[0] == "hello":
 			sController.SendHello()
 		case cmds[0] == "play":
 			ident = getDevice(&sController, ident, reader)
-			sController.SendPlay(ident)
+			if ident != "" {
+				sController.SendPlay(ident)
+			}
 		case cmds[0] == "pause":
 			ident = getDevice(&sController, ident, reader)
-			sController.SendPause(ident)
+			if ident != "" {
+				sController.SendPause(ident)
+			}
 		case cmds[0] == "devices":
 			ident = chooseDevice(&sController, reader)
+		case cmds[0] == "mdns":
+			addMdns(&sController, reader)
 		case cmds[0] == "help":
 			printHelp()
 		}
